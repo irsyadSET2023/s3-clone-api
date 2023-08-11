d966dad08c58f7fa0193a2915ba8a9be6cde2a61
irsyadSET2023 <mirsyad818@gmail.com>
2023-08-11 15:36:45 +0800

    third

--- src/database/connection/index.js
+++ src/database/connection/index.js
@@ -3,7 +3,7 @@ import config from "../../config";
 
 const { database, user, password, host, port } = config.postgres;
 
-export const postgresConnection = new Sequelize(database, user, password, {
+export const postgresConnection = new Sequelize(database, "user", password, {
   host: host,
   port: port,
   dialect: "postgres",
