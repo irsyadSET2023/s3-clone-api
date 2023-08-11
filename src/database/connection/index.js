79a88f43ee5a96a2a71543beb385d053701d7ffb
irsyadSET2023 <mirsyad818@gmail.com>
2023-08-11 15:12:03 +0800

    change database configuration

--- src/database/connection/index.js
+++ src/database/connection/index.js
@@ -1,22 +1,27 @@
 import { Sequelize } from "sequelize";
 import config from "../../config";
 
-const { database, user, password, host, port } = config.postgres;
+const data = config.postgres;
 
-export const postgresConnection = new Sequelize(database, user, password, {
-  host: host,
-  port: port,
-  dialect: "postgres",
-
-  dialectOptions: {
-    ssl:
-      config.nodeEnv === "production"
-        ? {
-            require: true,
-            rejectUnauthorized: false,
-          }
-        : false,
-  },
-});
+const postgresConnection = new Sequelize(
+  data.database,
+  data.username,
+  data.password,
+  {
+    host: data.host,
+    port: data.port,
+    dialect: "postgres",
+    logging: config.nodeEnv === "development",
+    dialectOptions: {
+      ssl:
+        config.nodeEnv === "production"
+          ? {
+              require: true,
+              rejectUnauthorized: false,
+            }
+          : false,
+    },
+  }
+);
 
 export default postgresConnection;
